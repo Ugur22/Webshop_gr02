1ca63cae1c0b826d46147c52c9700697bfe749a5
sebastiaans10 <sebastiaans10@gmail.com>
2015-04-11 13:26:04 +0200

    goldmember percentage en min bedrag wijzigen werkt nu

---
+++ Webshop_gr02/Models/GoldMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webshop_gr02.Models
+{
+    public class GoldMember
+    {
+
+
+        public int ID_GM { get; set; }
+        public float percentage { get; set; }
+        public float min_bedrag { get; set; }
+       
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", ID_GM, percentage, min_bedrag);
+        }
+    }
+}
