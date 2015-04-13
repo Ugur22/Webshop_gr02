c83c0704a3d47d4aee1d0ce51731008a096df5e4
Shakla Zovack <shakla@webwindow.nl>
2015-04-13 15:59:08 +0200

    OverzichtBestellingKlant

--- Webshop_gr02/Controllers/KlantController.cs
+++ Webshop_gr02/Controllers/KlantController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Webshop_gr02.DatabaseControllers;
+using Webshop_gr02.Models;
 
 namespace Webshop_gr02.Controllers
 {
@@ -10,11 +12,40 @@ namespace Webshop_gr02.Controllers
     {
         //
         // GET: /Klant/
+        private AuthDBController authDBController = new AuthDBController();
 
         public ActionResult Index()
         {
             return View();
         }
+
+        //public ActionResult OverzichtKlant()
+        //{
+        //    try
+        //    {
+        //        List<Klant> klant = authDBController.GetKlant();
+        //        return View(klant);
+
+
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        ViewBag.Foutmelding = "Er is iets fout gegeaan" + e;
+        //        return View();
+        //    }
+
+
+        //}
+
+        //[Authorize(Roles = "KLANT")]
+        //public ActionResult OverzichtKlant(string username)
+        //{
+        //    Klant klant = authDBController.getKlantGegevens(username);
+
+        //    return View(klant);
+        //}
+
+        
 
     }
 }
