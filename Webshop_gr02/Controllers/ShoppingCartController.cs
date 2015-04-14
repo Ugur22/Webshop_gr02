f6f4006ae1d12fed6ee9b349d4af531028447122
Ugur22 <14069504@student.hhs.nl>
2015-04-14 10:28:07 +0200

    commit

--- Webshop_gr02/Controllers/ShoppingCartController.cs
+++ Webshop_gr02/Controllers/ShoppingCartController.cs
@@ -1,62 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Webshop_gr02.Models;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+//using Webshop_gr02.Models;
 
-namespace Webshop_gr02.Controllers
-{
-    public class ShoppingCartController : Controller
-    {
+//namespace Webshop_gr02.Controllers
+//{
+//    public class ShoppingCartController : Controller
+//    {
 
-        ProductTypeEntities storeDB = new ProductTypeEntities();
+//        //ProductTypeEntities storeDB = new ProductTypeEntities();
 
-        public ActionResult Index()
-        {
+//        Product product = new Product();
 
-            return View();
-        }
+//        public ActionResult Index()
+//        {
 
-        private int isExisting(int id)
-        {
-            List<Item> cart = (List<Item>)Session["cart"];
-            for (int i = 0; i < cart.Count; i++)
-                if (cart[i].Product.ID_P == id)
-                    return i;
-            return -1;
-        }
+//            return View();
+//        }
 
-        public ActionResult Delete(int id)
-        {
-            int index = isExisting(id);
-            List<Item> cart = (List<Item>)Session["cart"];
-            cart.RemoveAt(index);
-            Session["cart"] = cart;
-            return View("Cart");
-        }
+//        private int isExisting(int id)
+//        {
+//            List<Item> cart = (List<Item>)Session["cart"];
+//            for (int i = 0; i < cart.Count; i++)
+//                if (cart[i].Product.ID_P == id)
+//                    return i;
+//            return -1;
+//        }
 
-        public ActionResult OrderNow(int id)
-        {
-            if (Session["cart"] == null)
-            {
-                List<Item> cart = new List<Item>();
-                cart.Add(new Item(storeDB.Product.Find(id), 1));
-                Session["cart"] = cart;
+//        public ActionResult Delete(int id)
+//        {
+//            int index = isExisting(id);
+//            List<Item> cart = (List<Item>)Session["cart"];
+//            cart.RemoveAt(index);
+//            Session["cart"] = cart;
+//            return View("Cart");
+//        }
 
-            }
-            else
-            {
-                List<Item> cart = (List<Item>)Session["cart"];
-                int index = isExisting(id);
-                if (index == -1)
-                    cart.Add(new Item(storeDB.producttypes.Find(id), 1));
-                else
-                    cart[index].Quantity++;
-                Session["cart"] = cart;
-            }
-            return View("Cart");
-        }
+//        public ActionResult OrderNow(int id)
+//        {
+//            if (Session["cart"] == null)
+//            {
+//                List<Item> cart = new List<Item>();
+//                cart.Add(new Item(Product.Find(id), 1));
+//                Session["cart"] = cart;
 
-    }
-}
+//            }
+//            else
+//            {
+//                List<Item> cart = (List<Item>)Session["cart"];
+//                int index = isExisting(id);
+//                if (index == -1)
+//                    cart.Add(new Item(Product.Find(id), 1));
+//                else
+//                    cart[index].Quantity++;
+//                Session["cart"] = cart;
+//            }
+//            return View("Cart");
+//        }
+
+//    }
+//}
